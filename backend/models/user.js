c0f553273dd201e25c4c169f222db076b28e8cef
Vimulatus <vasu.mit2501@gmail.com>
2023-06-22 11:42:01 +0530

    unique added back to the username field

--- backend/models/user.js
+++ backend/models/user.js
@@ -13,7 +13,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     userName: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     email: {
         type: String,
