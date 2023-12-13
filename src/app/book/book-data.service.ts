ab577ab8d88070a91ab4cecdacf4b7124890ce07
Vimulatus <vasu.mit2501@gmail.com>
2023-12-13 17:11:25 +0530

    Update book-data.service.ts
    removed api keys

--- src/app/book/book-data.service.ts
+++ src/app/book/book-data.service.ts
@@ -3,11 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Book, BookList } from './book';
 import { ToggleThemeService } from '../toggle-theme.service';
 
-const API_KEY = '&key=AIzaSyAK05tQcGZWegQXEuHThGEuTxpPO-HuRIY';
-const URI = 'https://books.googleapis.com/books/v1/volumes?q=';
-// const NYT_API_KEY = 'GoGQXVE6hUR8V4HI1jl4xW1HEelY8Gnc';
-// const NYT_URI = 'https://api.nytimes.com/svc/books/v3/';
-
 @Injectable({
   providedIn: 'root'
 })
